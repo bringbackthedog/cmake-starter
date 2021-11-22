30a79bd48b201e82378df4a2652d535bf733a893
Matt Williams <mattinzhuhai@outlook.com>
2021-11-22 10:52:16 -0800

    :octocat: Add CPP11

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,26 @@
 #include <fmt/color.h>
 
+#include <CLI/CLI.hpp>
 #include <functional>
 #include <iostream>
 #include <map>
 #include <starter/addition.hpp>
 #include <starter/version.hpp>
 
-#include "docopt.h"
-
-static constexpr auto USAGE =
-    R"(Naval Fate.
-    Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
- Options:
-          -h --help     Show this screen.
-          --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
-)";
+using namespace fmt;
 
 auto main(int argc, const char **argv) -> int32_t {
-  std::map<std::string, docopt::value> args =
-      docopt::docopt(USAGE, {std::next(argv), std::next(argv, argc)},
-                     true,               // show help if requested
-                     "Naval Fate 2.0");  // version string
+  auto getVersion = []() -> std::string {
+    std::string v{to_string(STARTER_VMAJOR)};
+    v += "." + std::to_string(STARTER_VMINOR) + "." +
+         std::to_string(STARTER_VPATCH);
+    return v;
+  };
 
-  for (auto const &arg : args) {
-    std::cout << arg.first << "=" << arg.second << '\n';
-  }
+  CLI::App app;
+  CLI11_PARSE(app, argc, argv);
 
-  std::cout << "Version: " << STARTER_VER_MAJOR << '\n';
-
-  auto sum = starter::add(2, 2);
-  fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold, "\n2 + 2 = {}!\n", sum);
+  print("Version: {}", getVersion());
+  print(fg(color::crimson) | emphasis::bold, "\n2 + 2 = {}!\n",
+        starter::add(2, 2));
 }
